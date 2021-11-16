089cf5f0f8d194aa46d5421c0de50848ac071358
The Matty <mattymatty@freemails.it>
2021-11-16 20:32:46 +0100

    V 1.7.1 - BETA Fixed crash loading Containers with loot tables

--- src/main/java/com/carpet_shadow/interfaces/InventoryItem.java
+++ src/main/java/com/carpet_shadow/interfaces/InventoryItem.java
@@ -1,6 +1,4 @@
 package com.carpet_shadow.interfaces;
-
-import com.carpet_shadow.CarpetShadowSettings;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
@@ -17,25 +15,26 @@ public interface InventoryItem {
     void removeSlot(Inventory inventory, int slot);
 
     static void readNbt(BlockEntity instance, NbtCompound nbt) {
-
         if (instance instanceof Inventory inv) {
-
-            for (int index = 0; index < inv.size(); index++) {
-                ItemStack stack = inv.getStack(index);
-                if (((ShadowItem) (Object) stack).getShadowId() != null) {
-                    ((InventoryItem) (Object) stack).removeSlot(inv, index);
+            try {
+                for (int index = 0; index < inv.size(); index++) {
+                    ItemStack stack = inv.getStack(index);
+                    if (((ShadowItem) (Object) stack).getShadowId() != null) {
+                        ((InventoryItem) (Object) stack).removeSlot(inv, index);
+                    }
                 }
-            }
+            }catch (Exception ignored){}
 
             instance.readNbt(nbt);
 
-            for (int index = 0; index < inv.size(); index++) {
-                ItemStack stack = inv.getStack(index);
-                if (((ShadowItem) (Object) stack).getShadowId() != null) {
-                    ((InventoryItem) (Object) stack).addSlot(inv, index);
+            try {
+                for (int index = 0; index < inv.size(); index++) {
+                    ItemStack stack = inv.getStack(index);
+                    if (((ShadowItem) (Object) stack).getShadowId() != null) {
+                        ((InventoryItem) (Object) stack).addSlot(inv, index);
+                    }
                 }
-            }
-
+            }catch (Exception ignored){}
         } else {
             instance.readNbt(nbt);
         }
--- src/main/java/com/carpet_shadow/mixins/inv_updates/loaders/BlockEntityMixin.java
+++ src/main/java/com/carpet_shadow/mixins/inv_updates/loaders/BlockEntityMixin.java
@@ -22,12 +22,14 @@ public abstract class BlockEntityMixin {
     @Inject(method = "createFromNbt",at=@At("RETURN"))
     private static void interceptBlockEntityLoad(BlockPos pos, BlockState state, NbtCompound nbt, CallbackInfoReturnable<BlockEntity> cir){
         if(cir.getReturnValue() instanceof Inventory inv){
-            for (int index = 0; index < inv.size(); index++) {
-                ItemStack stack = inv.getStack(index);
-                if (((ShadowItem) (Object) stack).getShadowId() != null) {
-                    ((InventoryItem) (Object) stack).addSlot(inv, index);
+            try {
+                for (int index = 0; index < inv.size(); index++) {
+                    ItemStack stack = inv.getStack(index);
+                    if (((ShadowItem) (Object) stack).getShadowId() != null) {
+                        ((InventoryItem) (Object) stack).addSlot(inv, index);
+                    }
                 }
-            }
+            }catch(Exception ignored){}
         }
     }
 
